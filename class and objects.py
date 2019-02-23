cffba5010586b1772ab877c0a5d04f8c7f195126
marklittle716 <marklittle716@gmail.com>
2019-02-23 18:53:13 -0500

    Class demo

---
+++ class and objects.py
@@ -0,0 +1,15 @@
+#creating a class
+
+class User:
+    def __init__(self, name, age, height, weight):
+        self.name = name
+        self.age = age
+        self.height = height
+        self.weight = weight
+
+Student1 = User('Tony', 39, 72, 209)
+
+print(Student1.age)
+print(Student1.name)
+
+
