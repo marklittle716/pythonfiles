b4f31919db84e3cf9f361ad74b537790c070e083
marklittle716 <marklittle716@gmail.com>
2019-02-23 18:13:47 -0500

    Create functions.py
    Refresher in if/else statements

---
+++ functions.py
@@ -0,0 +1,19 @@
+# A function is a block of code which only runs when it is called. In Python, we do not use parentheses and curly brackets, we use indentation with tabs or spaces
+
+
+
+# A lambda function is a small anonymous function.
+# A lambda function can take any number of arguments, but can only have one expression. Very similar to JS arrow functions
+
+#fun with functions
+
+num1 = input('enter a number ')
+num2 = input('enter a second number ')
+
+sum = int(num1) + int(num2)
+
+if sum <= 30:
+    print('The total is less than or equal to 30')
+else:
+
+    print(sum)
